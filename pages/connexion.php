2fe4ed44d10321a5d9dbf252b6ab9ebe269312c8
Mattéo <matteo.tremisi@viacesi.fr>
2023-02-27 09:32:55 +0100

    nouvelle page de connexion animée !
    brr

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,18 +1,29 @@
-<?php
-include("header.php");
-?>
-<form method="post" class="login">
-    <h1>Se connecter</h1>
-    <fieldset>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password">
-        <input type="submit" value="Se connecter">
-        <input type="reset" value="Réinitialiser">    
-    </fieldset> 
-</form>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/connexion2.css">
+    <title>Document</title>
 
-<?php
-include("footer.php");
-?>
+<div class="login-box">
+  <h2>Connexion</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Adresse mail</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Mot de passe</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Envoyer
+    </a>
+  </form>
+</div>
