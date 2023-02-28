8089c8b3e6595d6639a4fc062802da1834abec5d
Mattéo <matteo.tremisi@viacesi.fr>
2023-02-28 14:38:08 +0100

    COMMIT DE SUPPRESSION
    got suppressed by malzahar R

--- pages/header.php
+++ pages/header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/pico.min.css">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="./styles/pico.min.css">
+    <link rel="stylesheet" href="./styles/style.css">
     <title>Document</title>
 </head>
 <body>
     <nav>
        <!--<a href="/"><h1>C<p>esi</p>T<p>on</p>S<p>tage</p></h1></a>-->
-       <img src="../logoCTS.png" alt="logo">
+       <img src="./logoCTS.png" alt="logo">
         <ul>
             <li><a href="#">Link</a></li>
             <li><a href="#">Link</a></li>
