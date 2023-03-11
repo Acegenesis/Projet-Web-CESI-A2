580218dde06fbfa05110b2cea3218941fcde1a59
Mattéo <matteo.tremisi@viacesi.fr>
2023-03-11 17:24:05 +0100

    EDIT TRADUCTION

--- pages/signin.php
+++ pages/signin.php
@@ -13,7 +13,7 @@
                 <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/>
             </svg>
         </div>
-        <a href="signup.php"> Créer un compte ?</a>
-        <button>Se connecter</button>
+        <a href="signup.php"> No account ?</a>
+        <button>VALIDATE</button>
     </form>
 <?php include('../components/footer.php'); ?>
