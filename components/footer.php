eb8a0eb7820de97b85c0bdd0efe5019171e6c765
Mattéo <matteo.tremisi@viacesi.fr>
2023-03-13 11:21:15 +0100

    petit easter egg

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,6 @@
         <footer>
                 <p class="copyright">
-                        Copyright © 2023 CTS Inc. All rights reserved.
+                        Copyright © 2023 CTS Inc. All rights reserved.<a href=../assets/img/carre.mp3 download>#&zwnj;</a>
                 </p>
         </footer>
         <script src="../assets/js/script.js"></script>
